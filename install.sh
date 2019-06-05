881be0c2815e8671ef180c8db7cd22e81813c961
zombiefungus <divmermarlav@gmail.com>
2019-06-05 10:31:58 -0500

    include theme usage in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+rm ~/.taskrc
 ln -s $(pwd)/taskrc ~/.taskrc
 rm -rf ~/.task/hooks
 mkdir -p ~/.task
+echo 'include '$(pwd)/darkokai.theme >> ~/.taskrc
 ln -s $(pwd)/hooks ~/.task/hooks
 chmod +x ~/.task/hooks/*
